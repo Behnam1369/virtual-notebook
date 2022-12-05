1401eff666f80b0c927fcb49b57d3abda438b5fd
behnam1369 <behnam.aghaali@yahoo.com>
2022-12-05 23:57:50 +0330

    Create Error404 component

---
+++ src/components/Error404.js
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import style from './Error404.module.scss';
+
+export default function Error404() {
+  return (
+    <div className={style.div}>
+      <h1>ERROR 404</h1>
+      <h2>
+        Page not found. CLick here to go to the
+        {' '}
+        <NavLink to="/"> home page</NavLink>
+        .
+      </h2>
+    </div>
+  );
+}
